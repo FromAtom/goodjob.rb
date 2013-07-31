3bc1c7ab041c82d945ab1e6ef5eae648e6527139
Atom <fromatom@gmail.com>
2013-08-01 03:25:16 +0900

    change:send message when push Shift+F5

--- goodjob.rb
+++ goodjob.rb
@@ -2,8 +2,19 @@
 #!/usr/bin/env ruby
 
 require "socket"
+require "mac-event-monitor"
 
 #localhost:3535
 sock = TCPSocket.open("127.0.0.1", 3535)
-sock.write("HELLO")
+
+monitor = Mac::EventMonitor::Monitor.new
+
+monitor.add_listener(:key_down) do |event|
+  #send message when push Shift+F5
+  if(event.keycode == 96 && event.shift_key?) then
+    sock.write("HELLO")
+  end
+end
+monitor.run
+
 sock.close
